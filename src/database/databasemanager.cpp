b54c4e40de95a1839e3fa7bed09b772c6855eb0b
Robson-Rissato <robsonwr@rissato.com.br>
2025-11-19 20:23:22 -0300

    Multi World System

---
+++ src/database/databasemanager.cpp
@@ -0,0 +1,189 @@
+////////////////////////////////////////////////////////////////////////
+// Crystal Server - an opensource roleplaying game
+////////////////////////////////////////////////////////////////////////
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+////////////////////////////////////////////////////////////////////////
+
+#include "database/databasemanager.hpp"
+
+#include "config/configmanager.hpp"
+#include "lua/functions/core/libs/core_libs_functions.hpp"
+#include "lua/scripts/luascript.hpp"
+#include "game/game.hpp"
+
+namespace InternalDBManager {
+	int32_t extractVersionFromFilename(const std::string &filename) {
+		std::regex versionRegex(R"((\d+)\.lua)");
+		std::smatch match;
+
+		if (std::regex_search(filename, match, versionRegex) && match.size() > 1) {
+			return std::stoi(match.str(1));
+		}
+
+		return -1;
+	}
+}
+
+bool DatabaseManager::optimizeTables() {
+	Database &db = Database::getInstance();
+	std::ostringstream query;
+
+	query << "SELECT `TABLE_NAME` FROM `information_schema`.`TABLES` WHERE `TABLE_SCHEMA` = " << db.escapeString(g_configManager().getString(MYSQL_DB)) << " AND `DATA_FREE` > 0";
+	DBResult_ptr result = db.storeQuery(query.str());
+	if (!result) {
+		return false;
+	}
+
+	do {
+		std::string tableName = result->getString("TABLE_NAME");
+
+		query.str(std::string());
+		query << "OPTIMIZE TABLE `" << tableName << '`';
+
+		std::string tableResult;
+		if (db.executeQuery(query.str())) {
+			tableResult = "[Success]";
+		} else {
+			tableResult = "[Failed]";
+		}
+
+		g_logger().info("Optimizing table {}... {}", tableName, tableResult);
+	} while (result->next());
+
+	return true;
+}
+
+bool DatabaseManager::tableExists(const std::string &tableName) {
+	Database &db = Database::getInstance();
+
+	std::ostringstream query;
+	query << "SELECT `TABLE_NAME` FROM `information_schema`.`tables` WHERE `TABLE_SCHEMA` = " << db.escapeString(g_configManager().getString(MYSQL_DB)) << " AND `TABLE_NAME` = " << db.escapeString(tableName) << " LIMIT 1";
+	return db.storeQuery(query.str()).get() != nullptr;
+}
+
+bool DatabaseManager::isDatabaseSetup() {
+	Database &db = Database::getInstance();
+	std::ostringstream query;
+	query << "SELECT `TABLE_NAME` FROM `information_schema`.`tables` WHERE `TABLE_SCHEMA` = " << db.escapeString(g_configManager().getString(MYSQL_DB));
+	return db.storeQuery(query.str()).get() != nullptr;
+}
+
+int32_t DatabaseManager::getDatabaseVersion() {
+	if (!tableExists("server_config")) {
+		Database &db = Database::getInstance();
+		db.executeQuery("CREATE TABLE `server_config` (`config` VARCHAR(50) NOT NULL, `value` VARCHAR(256) NOT NULL DEFAULT '', `world_id` INT(11) NOT NULL DEFAULT '1', UNIQUE(`config`, `world_id`)) ENGINE = InnoDB");
+		db.executeQuery(fmt::format("INSERT INTO `server_config` (`config`, `value`) VALUES ('db_version', 0)"));
+		return 0;
+	}
+
+	int32_t version = 0;
+	if (getDatabaseConfig("db_version", version)) {
+		return version;
+	}
+	return -1;
+}
+
+void DatabaseManager::updateDatabase() {
+	Benchmark bm;
+	lua_State* L = luaL_newstate();
+	if (!L) {
+		return;
+	}
+
+	luaL_openlibs(L);
+	CoreLibsFunctions::init(L);
+
+	int32_t currentVersion = getDatabaseVersion();
+	std::string migrationDirectory = g_configManager().getString(CORE_DIRECTORY) + "/migrations/";
+
+	std::vector<std::pair<int32_t, std::string>> migrations;
+
+	for (const auto &entry : std::filesystem::directory_iterator(migrationDirectory)) {
+		if (entry.is_regular_file()) {
+			std::string filename = entry.path().filename().string();
+			int32_t fileVersion = InternalDBManager::extractVersionFromFilename(filename);
+			migrations.emplace_back(fileVersion, entry.path().string());
+		}
+	}
+
+	std::sort(migrations.begin(), migrations.end());
+
+	for (const auto &[fileVersion, scriptPath] : migrations) {
+		if (fileVersion > currentVersion) {
+			if (!LuaScriptInterface::reserveScriptEnv()) {
+				break;
+			}
+
+			if (luaL_dofile(L, scriptPath.c_str()) != 0) {
+				g_logger().error("DatabaseManager::updateDatabase - Version: {}] {}", fileVersion, lua_tostring(L, -1));
+				continue;
+			}
+
+			lua_getglobal(L, "onUpdateDatabase");
+			if (lua_pcall(L, 0, 1, 0) != 0) {
+				LuaScriptInterface::resetScriptEnv();
+				g_logger().warn("[DatabaseManager::updateDatabase - Version: {}] {}", fileVersion, lua_tostring(L, -1));
+				continue;
+			}
+
+			currentVersion = fileVersion;
+			g_logger().info("Database has been updated to version {}", currentVersion);
+			registerDatabaseConfig("db_version", currentVersion);
+
+			LuaScriptInterface::resetScriptEnv();
+		}
+	}
+
+	double duration = bm.duration();
+	if (duration < 1000.0) {
+		g_logger().debug("Database update completed in {:.2f} ms", duration);
+	} else {
+		g_logger().debug("Database update completed in {:.2f} seconds", duration / 1000.0);
+	}
+	lua_close(L);
+}
+
+bool DatabaseManager::getDatabaseConfig(const std::string &config, int32_t &value) {
+	Database &db = Database::getInstance();
+	std::string query = fmt::format("SELECT `value` FROM `server_config` WHERE `world_id` = {} AND `config` = {}", g_game().worlds().getCurrentWorld()->id, db.escapeString(config));
+	if (config == "db_version") {
+		query = fmt::format("SELECT `value` FROM `server_config` WHERE `config` = {}", db.escapeString(config));
+	}
+
+	const auto result = db.storeQuery(query);
+	if (!result) {
+		return false;
+	}
+
+	value = result->getNumber<int32_t>("value");
+	return true;
+}
+
+void DatabaseManager::registerDatabaseConfig(const std::string &config, int32_t value) {
+	Database &db = Database::getInstance();
+	std::string query;
+
+	int32_t tmp;
+
+	if (!getDatabaseConfig(config, tmp)) {
+		query = fmt::format("INSERT INTO `server_config` (`config`, `value`, `world_id`) VALUES ({}, {}, {})", db.escapeString(config), value, g_game().worlds().getCurrentWorld()->id);
+	} else {
+		query = fmt::format("UPDATE `server_config` SET `value` = {} WHERE `world_id` = {} AND `config` = {}", value, g_game().worlds().getCurrentWorld()->id, db.escapeString(config));
+		if (strcasecmp(config.c_str(), "db_version")) {
+			query = fmt::format("UPDATE `server_config` SET `value` = {} WHERE `config` = {}", value, db.escapeString(config));
+		}
+	}
+
+	db.executeQuery(query);
+}
